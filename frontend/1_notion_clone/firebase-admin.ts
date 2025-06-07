1ae1e74cc0196cafa50cf4d50419125dda2a4bf6
Dani <daniel.dkh822@gmail.com>
2025-06-07 20:58:46 +0330

    Refactor firebase-admin to use environment variables fixed

--- frontend/1_notion_clone/firebase-admin.ts
+++ frontend/1_notion_clone/firebase-admin.ts
@@ -1,17 +1,17 @@
-import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
+import { initializeApp, getApps, cert, App, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = {
+const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // <- IMPORTANT
 };
 
 let app: App;
 
-if (getApps().length === 0) {
+if (!getApps().length) {
   app = initializeApp({
-    credential: cert(serviceKey),
+    credential: cert(serviceAccount as any),
   });
 } else {
   app = getApp();
@@ -19,4 +19,4 @@ if (getApps().length === 0) {
 
 const adminDb = getFirestore(app);
 
-export { app as adminApp, adminDb };
+export { adminDb, app as adminApp };
