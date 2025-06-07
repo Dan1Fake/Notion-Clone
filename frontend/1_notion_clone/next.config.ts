0b5c8a507c207a87099941b80f47971934dc7400
Dani <daniel.dkh822@gmail.com>
2025-06-07 22:19:06 +0330

    idkv3

--- frontend/1_notion_clone/next.config.ts
+++ frontend/1_notion_clone/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    nodeMiddleware: true,  // keep this to enable Node.js middleware runtime
-  },
+
 };
 
 export default nextConfig;
