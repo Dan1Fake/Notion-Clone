966d738ceb9190cba6c7970ca17638f6c1352890
Dani <daniel.dkh82@gmail.com>
2026-01-28 14:19:17 +0330

    Updated the function that creates users mouse cursor's color.

--- frontend/1_notion_clone/lib/stringToColor.ts
+++ frontend/1_notion_clone/lib/stringToColor.ts
@@ -4,7 +4,7 @@ function stringToColor(str: string) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
 
-  const c = (hash & 0x00fffff).toString(16).toUpperCase();
+  const c = (hash & 0x00ffffff).toString(16).toUpperCase();
   return "#" + "00000".substring(0, 6 - c.length) + c;
 }
 
